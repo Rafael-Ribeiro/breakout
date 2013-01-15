0c4b42fc985f7e5978c6cf1bcc8538ae2f0dfa98
José Ribeiro <jlbribeiro@gmail.com>
2013-01-15 18:56:03 +0000

    [src] prototyping bricks

--- project/src/impl/BreakoutWorld.hpp
+++ project/src/impl/BreakoutWorld.hpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <sys/time.h>
 
+#include <map>
+
 #include "jsoncpp/inc/json.h"
 #include "CPUStrategyMultiton.hpp"
 
@@ -363,21 +365,12 @@ bool BreakoutWorld::load_level(string level_filename_path)
 	if (!parsingSuccessful)
 		return false;
 
-	Json::Value bricks = level["bricks"];
-	Json::Value map = level["map"];
-	Json::Value balls = level["balls"];
+	Json::Value _bricks = level["bricks"];
+	Json::Value _map = level["map"];
+	Json::Value _balls = level["balls"];
 
-	if (!bricks || !bricks.isObject() || !map || !map.isArray() || !balls || !balls.isArray())
+	if (!_bricks || !_bricks.isObject() || !_map || !_map.isArray() || !_balls || !_balls.isArray())
 		return false;
-
-	// vector<string> brick_types = bricks.getMemberNames();
-	// for (unsigned int i = 0; i < brick_types.size(); i++)
-	// {
-	// 	string alias = brick_types[i];
-	// 	string type = bricks[brick_types[i]]["type"].asString();
-	// 	string color = bricks[brick_types[i]]["color"].asString();
-	// 	unsigned int hits = bricks[brick_types[i]].get("hits", 1).asUInt();
-	// }
 
 	const string NO_BRICK = string(1, ' ');
 	const string NORMAL_BRICK("normal");
@@ -394,59 +387,63 @@ bool BreakoutWorld::load_level(string level_filename_path)
 	const double X_OFFSET = (BreakoutWorld::WIDTH - (X_BLOCKS - 1) * Brick::BASE_WIDTH) / 2;
 	const double Y_OFFSET = (BreakoutWorld::HEIGHT - (Y_BLOCKS - 1) * Brick::BASE_HEIGHT) / 2;
 
-	for (unsigned int i = 0; i < map.size(); i++)
+	map<string, Brick *> brick_prototypes;
+
+	vector<string> brick_types = _bricks.getMemberNames();
+	for (unsigned int i = 0; i < brick_types.size(); i++)
 	{
-		string map_line = map[i].asString();
+		string alias = brick_types[i];
+		string brick_type = _bricks[alias]["type"].asString();
+
+		if (!brick_type.compare(NORMAL_BRICK))
+		{
+			unsigned int hits = _bricks[alias].get("hits", 1).asUInt();
+			string color = _bricks[alias].get("color", "white").asString();
+
+			brick_prototypes[alias] = BrickFactory::make_normal_brick(Point(0.0), QColor(color.c_str()), hits);
+
+		} else if (!brick_type.compare(GLASS_BRICK))
+			brick_prototypes[alias] = BrickFactory::make_glass_brick(Point(0.0));
+
+		else if (!brick_type.compare(CONCRETE_BRICK))
+			brick_prototypes[alias] = BrickFactory::make_concrete_brick(Point(0.0));
+
+	}
+
+	for (unsigned int i = 0; i < _map.size(); i++)
+	{
+		string map_line = _map[i].asString();
 
 		unsigned int y = i * Brick::BASE_HEIGHT;
 
 		for (unsigned int j = 0; j < map_line.length(); j++)
 		{
-			string brick_type_alias = string(1, map_line[j]);
+			string alias = string(1, map_line[j]);
 
-			if (!brick_type_alias.compare(NO_BRICK))
+			if (!alias.compare(NO_BRICK))
 				continue;
 
 			unsigned int x = j * Brick::BASE_WIDTH;
-			Point position = Point(X_OFFSET + x, Y_OFFSET + y);
 
-			string brick_type = bricks[brick_type_alias]["type"].asString();
-			if (!brick_type.compare(NORMAL_BRICK))
-			{
-				unsigned int hits = bricks[brick_type_alias].get("hits", 1).asUInt();
-				string color = bricks[brick_type_alias].get("color", "white").asString();
+			Brick *brick = brick_prototypes[alias]->clone();
+			brick->position() = Point(X_OFFSET + x, Y_OFFSET + y);
 
-				Brick *brick = BrickFactory::make_normal_brick(position, QColor(color.c_str()), hits);
-				this->add(brick);
-
-			} else if (!brick_type.compare(GLASS_BRICK))
-			{
-				Brick *brick = BrickFactory::make_glass_brick(position);
-				this->add(brick);
-
-			} else if (!brick_type.compare(CONCRETE_BRICK))
-			{
-				Brick *brick = BrickFactory::make_concrete_brick(position);
-				this->add(brick);
-
-			} else
-				return false;
-
+			this->add(brick);
 		}
 	}
 
-	for (unsigned int i = 0; i < balls.size(); i++)
+	for (unsigned int i = 0; i < _balls.size(); i++)
 	{
 		// unsigned int radius = balls[i].get("radius", DEFAULT_BALL_RADIUS).asUInt();
 		// string color = balls[i]["color"].asString();
 
-		double px = balls[i]["position"].get("x", 0.0).asDouble();
-		double py = balls[i]["position"].get("y", 0.0).asDouble();
+		double px = _balls[i]["position"].get("x", 0.0).asDouble();
+		double py = _balls[i]["position"].get("y", 0.0).asDouble();
 
-		double vx = balls[i]["velocity"].get("x", 0.0).asDouble();
-		double vy = balls[i]["velocity"].get("y", 0.0).asDouble();
+		double vx = _balls[i]["velocity"].get("x", 0.0).asDouble();
+		double vy = _balls[i]["velocity"].get("y", 0.0).asDouble();
 
-		string ball_type = balls[i]["type"].asString();
+		string ball_type = _balls[i]["type"].asString();
 		Point position(px, py);
 
 		Vector velocity;
